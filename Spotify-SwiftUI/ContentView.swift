1537d89e96f7c212090ef578193ead7cb910d88e
Oguzhnblt <oguzhanbolat0@gmail.com>
2024-05-11 17:40:23 +0300

    Initial Commit

---
+++ Spotify-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spotify-SwiftUI
+//
+//  Created by Oğuz on 11.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spotify-SwiftUI/Spotify_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_SwiftUIApp.swift
+//  Spotify-SwiftUI
+//
+//  Created by Oğuz on 11.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
